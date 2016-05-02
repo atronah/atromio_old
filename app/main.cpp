569e03aabb9171c4c1eaa0253c46d4332ab55457
atronah <atronah.ds@gmail.com>
2016-05-03 00:17:05 +0300

    [AD]add init_database call into main() function

--- app/main.cpp
+++ app/main.cpp
@@ -9,6 +9,8 @@
     #include <Money.h>
 #endif
 
+#include <Controller.h>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -16,6 +18,9 @@ int main(int argc, char *argv[])
     #ifdef A_TEST_DEL
         qWarning() << lldiv(11234, 100).rem;
     #endif
+
+    Controller c;
+    c.init_database();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
